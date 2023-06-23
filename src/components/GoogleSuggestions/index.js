9cf8bca44925135c2dcbdd38e4d9f7da5d0a1cf6
ShubhamBthakre <132575491+ShubhamBthakre@users.noreply.github.com>
2023-06-24 01:18:35 +0530

    update index.js

--- src/components/GoogleSuggestions/index.js
+++ src/components/GoogleSuggestions/index.js
@@ -1,61 +1,61 @@
-// Write your code here
-
 import {Component} from 'react'
-
 import SuggestionItem from '../SuggestionItem'
 
 import './index.css'
 
 class GoogleSuggestions extends Component {
-  state = {searchInput: ''}
-
-  onChangeInput = event => {
-    this.setState({searchInput: event.target.value})
+  state = {
+    searchInput: '',
   }
 
-  onClickSuggestionArrow = suggestion => {
+  updateSearchInput = suggestion => {
     this.setState({searchInput: suggestion})
+  }
+
+  onChangeSearchInput = event => {
+    this.setState({
+      searchInput: event.target.value,
+    })
   }
 
   render() {
     const {searchInput} = this.state
     const {suggestionsList} = this.props
-
-    const filteredSuggestionList = suggestionsList.filter(eachSuggestion =>
+    const searchResults = suggestionsList.filter(eachSuggestion =>
       eachSuggestion.suggestion
         .toLowerCase()
         .includes(searchInput.toLowerCase()),
     )
 
     return (
-      <div className="google-Suggestions-container">
-        <div className="cart-container">
+      <div className="app-container">
+        <div className="google-suggestions-container">
           <img
             src="https://assets.ccbp.in/frontend/react-js/google-logo.png"
             alt="google logo"
             className="google-logo"
           />
-          <div className="suggestion-item-bg-container">
-            <div className="input-container">
+          <div className="search-input-suggestions-container">
+            <div className="search-input-container">
               <img
-                src="https://assets.ccbp.in/frontend/react-js/google-search-icon.png "
                 alt="search icon"
                 className="search-icon"
+                src="https://assets.ccbp.in/frontend/react-js/google-search-icon.png"
               />
               <input
-                className="input-element"
-                placeholder="Search Google"
-                onChange={this.onChangeInput}
-                value={searchInput}
                 type="search"
+                className="search-input"
+                placeholder="Search Google"
+                onChange={this.onChangeSearchInput}
+                value={searchInput}
               />
             </div>
-            <ul className="suggestion-items-container">
-              {filteredSuggestionList.map(suggestion => (
+            <ul className="suggestions-list">
+              {searchResults.map(eachSuggestion => (
                 <SuggestionItem
-                  suggestionItem={suggestion}
-                  onClickSuggestionArrow={this.onClickSuggestionArrow}
-                  key={suggestion.id}
+                  key={eachSuggestion.id}
+                  suggestionDetails={eachSuggestion}
+                  updateSearchInput={this.updateSearchInput}
                 />
               ))}
             </ul>
